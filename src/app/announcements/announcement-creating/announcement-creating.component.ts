30e181696869288dca64c6ce5221058557c99430
NazarDobriy <nazaranddobriy@gmail.com>
2022-04-11 16:50:37 +0300

    create components

---
+++ src/app/announcements/announcement-creating/announcement-creating.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement-creating',
+  templateUrl: './announcement-creating.component.html',
+  styleUrls: ['./announcement-creating.component.scss']
+})
+export class AnnouncementCreatingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnnouncementsListComponent } from './announcements/announcements-list/announcements-list.component';
+import { AnnouncementCreatingComponent } from './announcements/announcement-creating/announcement-creating.component';
+import { AnnouncementDeletingComponent } from './announcements/announcement-deleting/announcement-deleting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnnouncementsListComponent,
+    AnnouncementCreatingComponent,
+    AnnouncementDeletingComponent
   ],
   imports: [
     BrowserModule,
