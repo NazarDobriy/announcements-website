16bcc8d45465857797627d8efc409255cb9a1ba0
NazarDobriy <nazaranddobriy@gmail.com>
2022-04-13 13:19:04 +0300

    add similar announcements

--- src/app/announcements/announcements-list/announcements-list.component.ts
+++ src/app/announcements/announcements-list/announcements-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { IAnnouncement } from 'src/app/models/announcement.interface';
 import { AnnouncementService } from 'src/app/services/announcement.service';
@@ -9,16 +10,19 @@ import { AnnouncementService } from 'src/app/services/announcement.service';
   styleUrls: ['./announcements-list.component.scss']
 })
 export class AnnouncementsListComponent implements OnInit {
+  readonly SIMILAR_LIMIT: number = 3;
   public searchTitle: string = '';
   public announcements!: IAnnouncement[];
   public loading: boolean = false;
   public isAnnouncement: boolean = false;
+  public copyAnnouncements!: IAnnouncement[];
 
   constructor(private announcementService: AnnouncementService, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.announcementService.getAllAnnouncements().subscribe((responce: IAnnouncement[]) => {
       this.announcements = responce;
+      this.copyAnnouncements = responce;
       this.loading = true;
       if (this.announcements.length) {
         this.isAnnouncement = true;
@@ -45,4 +49,44 @@ export class AnnouncementsListComponent implements OnInit {
       verticalPosition: 'top'
     });
   }
+
+  public isSimilarText(str1: string, str2: string): boolean {
+    let result: boolean = false;
+    str2.split(' ').forEach((word) => {
+      if (str1.includes(word)) {
+        result = true;
+      }
+    });
+    return result;
+  }
+
+  public filterSimilarAnnouncements(): IAnnouncement[] {
+    const similarAnnouncements: IAnnouncement[] = [];
+    const uniqueAnnouncements: Set<number> = new Set();
+    for (let i = 0; i < this.announcements.length; i++) {
+      for (let j = i + 1; j < this.announcements.length; j++) {
+        const isSimilarTitle: boolean = this.isSimilarText(
+          this.announcements[i].title.toLocaleLowerCase(), 
+          this.announcements[j].title.toLocaleLowerCase()
+        );
+        const isSimilarDescription: boolean = this.isSimilarText(
+          this.announcements[i].description.toLocaleLowerCase(), 
+          this.announcements[j].description.toLocaleLowerCase()
+        );
+        if (isSimilarTitle && isSimilarDescription) {
+          uniqueAnnouncements.add(i);
+          uniqueAnnouncements.add(j);
+        }
+      }
+    }
+    for (const unique of uniqueAnnouncements) {
+      similarAnnouncements.push(this.announcements[unique]);
+    }
+    return similarAnnouncements.slice(0, this.SIMILAR_LIMIT);
+  }
+
+  public changeCheckBox(event: MatCheckboxChange): void {
+    event.checked ? this.announcements = this.filterSimilarAnnouncements() : this.announcements = this.copyAnnouncements;
+  }
+
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 const MaterialComponents = [
   MatProgressSpinnerModule,
   BrowserAnimationsModule,
-  MatSnackBarModule
+  MatSnackBarModule,
+  MatCheckboxModule
 ];
 
 @NgModule({
